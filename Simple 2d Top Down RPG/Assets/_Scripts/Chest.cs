3bc547cb25eee25d4462fe86e02a5b80b174a0f9
Stephen West <stevewestinlondon@gmail.com>
2023-11-23 22:04:47 +0000

    added a fade to the chest

--- Simple 2d Top Down RPG/Assets/_Scripts/Chest.cs
+++ Simple 2d Top Down RPG/Assets/_Scripts/Chest.cs
@@ -1,11 +1,14 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class Chest : MonoBehaviour
 {
+    public Sprite emptyChest;
+    public AudioSource PickupCoinSound;
     private SpriteRenderer spriteRenderer;
     private readonly float TimeDelay = 0.5f;
-    public Sprite emptyChest;
+    private readonly float fadeDuration = 0.5f;
 
     private void Awake()
     {
@@ -18,14 +21,45 @@ public class Chest : MonoBehaviour
         if (collision.CompareTag(PlayerController.Tag))
         {
             Debug.Log("Chest Hit");
-            Invoke(nameof(delayedDestroy), TimeDelay);
             GetComponent<Collider2D>().enabled = false;
+            GetComponent<AudioSource>().Play();
             spriteRenderer.sprite = emptyChest;
+            StartCoroutine(FadeObject());
         }
     }
 
     private void delayedDestroy()
     {
         Destroy(gameObject);
+    }
+
+
+    IEnumerator FadeObject()
+    {
+ 
+        Color startColor = spriteRenderer.material.color;
+
+        // Set the target color with initial alpha set to 0
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        // Time elapsed during the fade
+        float currentTime = 0.0f;
+
+        while (currentTime < fadeDuration)
+        {
+
+            currentTime += Time.deltaTime;
+            float lerpFactor = currentTime / fadeDuration;
+            spriteRenderer.material.color = Color.Lerp(startColor, targetColor, lerpFactor);
+
+            // Wait for the next frame
+            yield return null;
+        }
+
+        // Ensure the final color is set
+        spriteRenderer.material.color = targetColor;
+        Destroy(gameObject);
+
+
     }
 }
