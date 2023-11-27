eed4de7990e01c014cedd507572f833afeb07783
Stephen West <stevewestinlondon@gmail.com>
2023-11-27 04:31:24 +0000

    add direction sprite flip

--- Simple 2d Top Down RPG/Assets/_Scripts/Movement.cs
+++ Simple 2d Top Down RPG/Assets/_Scripts/Movement.cs
@@ -5,10 +5,12 @@ using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Movement : MonoBehaviour
 {
     Collider2D box;
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
     [SerializeField] protected float speed = 1f;
 
@@ -17,11 +19,31 @@ public class Movement : MonoBehaviour
     {
         box = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
+
 
     protected void MoveDir(Vector2 dir)
     {
         transform.Translate(Time.deltaTime * dir * speed);
+        CheckFlipSprite(dir);
     }
 
+    /// <summary>
+    /// Check weather the Movement spite is need to be fliped based on the direction dir.x
+    /// </summary>
+    /// <param name="dir">objects movement directions</param>
+    private void CheckFlipSprite(Vector2 dir)
+    {
+        // flip sprite if moving to left
+        if (dir.x < 0)
+        {
+            sr.flipX = true;
+        }
+        if (dir.x > 0)
+        {
+            sr.flipX = false;
+        }
+        // dir.x == 0 do nougthing
+    }
 }
