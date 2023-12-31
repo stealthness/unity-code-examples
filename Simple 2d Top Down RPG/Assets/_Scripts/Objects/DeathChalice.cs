331a01440c9532abbe6d5ccea24652e3bb8fdade
Stephen West <stevewestinlondon@gmail.com>
2023-12-31 17:54:15 +0000

    add instant Death chalice

---
+++ Simple 2d Top Down RPG/Assets/_Scripts/Objects/DeathChalice.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DeathChalice : Collidable
+{
+
+    public UnityEvent OnTouch;
+
+    protected override void OnCollide(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Debug.Log("Player Hit death Chalice");
+            OnTouch.Invoke();
+        }
+    }
+}
