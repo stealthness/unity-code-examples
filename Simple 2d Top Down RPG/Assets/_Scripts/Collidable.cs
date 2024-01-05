e07aa72e739e4cc3e8a1a2ae0cd6ff64a8a85fd8
Stephen West <stevewestinlondon@gmail.com>
2024-01-05 03:50:44 +0000

    commented out OnCollideEnter/Exit

--- Simple 2d Top Down RPG/Assets/_Scripts/Collidable.cs
+++ Simple 2d Top Down RPG/Assets/_Scripts/Collidable.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
@@ -7,13 +5,14 @@ public class Collidable : MonoBehaviour
 {
     [Tooltip("Layers that will be checked for collisions")]
     public ContactFilter2D ContactFilter;
-    [Tooltip("Layers that will be checked for collisions entered and exiting")]
-    public ContactFilter2D ContactFilterTracking;
+/*    [Tooltip("Layers that will be checked for collisions entered and exiting")]
+    public ContactFilter2D ContactFilterTracking;*/
 
     protected BoxCollider2D _box;
     protected bool _isCollidable = true; 
-    private readonly Collider2D[] _collidersEntered = new Collider2D[10];
     private readonly Collider2D[] _hits = new Collider2D[10];
+/*    private readonly Collider2D[] _collidersEntered = new Collider2D[10];
+    private readonly Collider2D[] _trackedHits = new Collider2D[10];*/
 
 
     protected virtual void Awake()
@@ -32,35 +31,38 @@ public class Collidable : MonoBehaviour
 
         // deal with contact
         _box.OverlapCollider(ContactFilter, _hits);
+
         for(int i  = 0; i < _hits.Length; i++)
         {
             if (_hits[i] == null)
             {
                 continue;
             }
-            Debug.Log($"Collidable: Update: _hits[{i}] = {_hits[i]}");
-            OnCollide(_hits[i]);
-
-
-            if (_collidersEntered.Contains(_hits[i]))
-            {
-                OnCollideEnter(_hits[i]);
-            }
-            else
-            {
-                Debug.Log("Not on list");
-            }
-            
+            OnCollide(_hits[i]);            
             _hits[i] = null;
         }
 
+/*        _box.OverlapCollider(ContactFilterTracking, _trackedHits);
+        for (int i = 0; i < _trackedHits.Length; i++)
+        {
+            if (_trackedHits[i] == null)
+            {
+                continue;
+            }
+            if (!_collidersEntered.Contains(_trackedHits[i]))
+            {
+                _collidersEntered[i] = _trackedHits[i];
+                OnCollideEnter(_trackedHits[i]);
+            }
+            _trackedHits[i] = null;
+        }  */ 
     }
 
     protected virtual void OnCollide(Collider2D collider)
     {
     }
 
-    protected virtual void OnCollideEnter(Collider2D collision)
+/*    protected virtual void OnCollideEnter(Collider2D collision)
     {
         if (!_isCollidable || _collidersEntered.Length == 0)
         {
@@ -74,5 +76,5 @@ public class Collidable : MonoBehaviour
         {
             return;
         }
-    }
+    }*/
 }
