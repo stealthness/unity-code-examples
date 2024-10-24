0feef5e0191083a7237cecf85d2bc30db0f940d3
Stealthness <stevewestinlondon@gmail.com>
2024-10-24 09:46:40 +0100

    Added Scrolling Background

---
+++ Simple Tappy Game/Assets/_Scripts/ScrollingBackground.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public class ScrollingBackground : MonoBehaviour
+    {
+        [SerializeField] private float scrollSpeed = 2f;
+    
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(scrollSpeed * Time.deltaTime * Vector3.left);
+            
+            
+            if (transform.position.x < -10)
+            {
+                transform.position = Vector3.zero;
+            }
+        }
+    }
+
+}
+
