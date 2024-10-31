678b363bdf9ac812267432e1073c88a018388a68
Stealthness <stevewestinlondon@gmail.com>
2024-10-31 18:04:41 +0000

    added docs

--- Simple 2D Top Down Shooter/Assets/_Scripts/Core/Movement2D.cs
+++ Simple 2D Top Down Shooter/Assets/_Scripts/Core/Movement2D.cs
@@ -3,23 +3,38 @@ using UnityEngine;
 
 namespace _Scripts.Core
 {
-    
+    /// <summary>
+    /// This class is the base class for all 2D movement scripts.
+    /// </summary>
     [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(BoxCollider2D))]
-    public class Movement2D : MonoBehaviour
+    public abstract class Movement2D : MonoBehaviour
     {
+        /// <summary>
+        /// A reference to the Rigidbody2D component and is accessed by the derived classes.
+        /// </summary>
         protected Rigidbody2D Rb;
-
+        /// <summary>
+        /// A vector that represents the direction of movement and is accessed by the derived classes.
+        /// </summary>
         internal Vector2 MovementDirection;
-
+        /// <summary>
+        /// A default speed value that can be changed in the inspector.
+        /// </summary>
         public float speed = 1f;
 
+        /// <summary>
+        /// Awake is called when the script instance is being loaded and will reference the Rigidbody2D component.
+        /// </summary>
         internal void Awake()
         {
             Rb = GetComponent<Rigidbody2D>();
         }
         
-
+        /// <summary>
+        /// Update is called once per frame and will move the object in the direction of the MovementDirection vector,
+        /// and is accessed by the derived classes.
+        /// </summary>
         protected void Update()
         {
             transform.position += (Vector3) MovementDirection * (speed * Time.deltaTime);
