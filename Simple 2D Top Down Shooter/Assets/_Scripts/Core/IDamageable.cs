397b9f8005d47830d79156a124b2cd1a85ddce60
Stealthness <stevewestinlondon@gmail.com>
2024-10-31 17:28:44 +0000

    added docs and SerialisedFields to FastEnemy.cs and IDamageable.cs

--- Simple 2D Top Down Shooter/Assets/_Scripts/Core/IDamageable.cs
+++ Simple 2D Top Down Shooter/Assets/_Scripts/Core/IDamageable.cs
@@ -1,11 +1,24 @@
 ﻿namespace _Scripts.Core
 {
+    /// <summary>
+    /// This interface is used to define the methods that a damageable object should have.
+    /// </summary>
     public interface IDamageable
     {
+        /// <summary>
+        /// This method is used to take damage.
+        /// </summary>
         public void TakeDamage(int damage);
         
+        /// <summary>
+        /// This method is used when the object dies.
+        ///  </summary>
         public void Die();
         
+        /// <summary>
+        /// Heals the object by the healAmount
+        /// </summary>
+        /// <param name="healAmount"></param>
         public void Heal(int healAmount);
     }
 }
--- Simple 2D Top Down Shooter/Assets/_Scripts/Enemy/FastEnemy.cs
+++ Simple 2D Top Down Shooter/Assets/_Scripts/Enemy/FastEnemy.cs
@@ -2,27 +2,47 @@
 
 namespace _Scripts.Enemy
 {
+    /// <summary>
+    /// This is fast Enemy class, it will move faster than a normal enemy but will pause for a second after every 2 seconds
+    /// </summary>
     public class FastEnemy : Enemy
     {
+        [SerializeField] private float pauseTime = 1f;
+        [SerializeField] private float movementPatternRepeatRate = 1f;
+        [SerializeField] private float fastEnemyMovementSpeed = 3f;
+        [SerializeField] private float fastEnemyPauseTime = 1f;
+        
+        /// <summary>
+        /// Overriding the start method to set the speed of the enemy and invoke the movement pattern
+        /// </summary>
         protected override void Start()
         {
             base.Start();
-            _enemyMovement.speed = 3f;
-            InvokeRepeating(nameof(MovementPattern), 0f, 2f);
+            _enemyMovement.speed = fastEnemyMovementSpeed;
+            InvokeRepeating(nameof(MovementPattern), 0f, movementPatternRepeatRate);
         }
         
+        /// <summary>
+        /// The movement pattern of the enemy, it will move for 1 second and then pause for 1 second
+        /// </summary>
         private void MovementPattern()
         {   
-            _enemyMovement.speed = 3f;
-            Invoke(nameof(PauseEnemy), 1f);
+            _enemyMovement.speed = fastEnemyMovementSpeed;
             _enemyMovement.ChangeDirection();
+            
+            if (fastEnemyPauseTime < movementPatternRepeatRate)
+            {
+                Invoke(nameof(PauseEnemy), fastEnemyPauseTime);
+            }
         }
         
+        /// <summary>
+        /// Pauses the enemy for 1 second
+        /// </summary>
         private void PauseEnemy()
         {
             _enemyMovement.MovementDirection = Vector2.zero;
             _enemyMovement.speed = 0f;
         }
-
     }
 }
