135accf7184f6f3a82615c7c014134eb94e8fd9e
Stealthness <stevewestinlondon@gmail.com>
2024-10-31 17:42:00 +0000

    doc and refactored using serialiseFields created

--- Simple 2D Top Down Shooter/Assets/_Scripts/Managers/EnemyManager.cs
+++ Simple 2D Top Down Shooter/Assets/_Scripts/Managers/EnemyManager.cs
@@ -2,13 +2,32 @@
 
 namespace _Scripts.Managers
 {
+    /// <summary>
+    /// EnemyManager is a singleton class that manages the generation of enemies.   
+    /// </summary>
     public class EnemyManager : MonoBehaviour
     {
      
         public static EnemyManager Instance;
-        public GameObject[] EnemyPrefab;
+        /// <summary>
+        /// List of the enemy prefabs
+        /// </summary>
+        public GameObject[] enemyPrefab;
+        /// <summary>
+        /// The target of the enemies
+        /// </summary>
         public Transform target;
+        /// <summary>
+        /// The frequency of generating enemies
+        /// </summary>
+        [SerializeField] private float generateEnemyFrequency = 1f;
         
+        [SerializeField] private float maxDistanceFromTargetToSpawn = 15f;
+        [SerializeField] private float minDistanceFromTargetToSpawn = 5f;
+
+        /// <summary>
+        /// Awake method to make sure that there is only one instance of the class
+        /// </summary>
         private void Awake()
         {
             if (Instance == null)
@@ -21,33 +40,48 @@ namespace _Scripts.Managers
             }
         }
         
-        
+        /// <summary>
+        /// Start method to start generating enemies
+        /// </summary>
         private void Start()
         {
-            InvokeRepeating(nameof(GenerateEnemy), 1, 1);
+            InvokeRepeating(nameof(GenerateEnemy), 0, generateEnemyFrequency);
         }
         
+        /// <summary>
+        /// Generate enemy method to generate enemies at random positions
+        /// </summary>
         private void GenerateEnemy()
         {
-            Instantiate(EnemyPrefab[0], validPostion(), Quaternion.identity);
-            Instantiate(EnemyPrefab[1], validPostion(), Quaternion.identity);
+            Instantiate(enemyPrefab[0], ValidPostion(), Quaternion.identity);
+            Instantiate(enemyPrefab[1], ValidPostion(), Quaternion.identity);
         }
 
-        private Vector3 validPostion()
+        /// <summary>
+        /// Method to get a valid position for the enemy, prevents the enemy from spawning near the target
+        /// </summary>
+        private Vector3 ValidPostion()
         {
-            Vector3 position = Vector3.zero;
-            bool notValid = true;
+            var randomStartPosition = Vector3.zero;
+            var notValid = true;
             while (notValid)
             {
-                position = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
-                if (Vector3.Distance(position, target.position) > 5)
+                // get a random position
+                var rx = Random.Range(-maxDistanceFromTargetToSpawn, maxDistanceFromTargetToSpawn);
+                var ry = Random.Range(-maxDistanceFromTargetToSpawn, maxDistanceFromTargetToSpawn);
+                randomStartPosition = new Vector3(rx, ry, 0);
+                // check if the distance between the target and the random position is greater than the minimum distance
+                if (Vector3.Distance(randomStartPosition, target.position) > minDistanceFromTargetToSpawn)
                 {
                     notValid = false;
                 }
             }
-            return position;
+            return randomStartPosition;
         }
-
+        
+        /// <summary>
+        /// Method to get the target of the enemies
+        /// </summary>
         public Vector3 GetTarget()
         {
             return target.position;
