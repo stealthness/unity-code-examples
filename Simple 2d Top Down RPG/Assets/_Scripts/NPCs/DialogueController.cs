175cbbc1b7801a9566db64b99005e9a098734a93
Stealthness <stevewestinlondon@gmail.com>
2024-12-13 18:48:11 +0000

    added typing effect

--- Simple 2d Top Down RPG/Assets/_Scripts/NPCs/DialogueController.cs
+++ Simple 2d Top Down RPG/Assets/_Scripts/NPCs/DialogueController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,11 +9,14 @@ namespace _Scripts.NPCs
     {
         [SerializeField] private TextMeshProUGUI npcNameText;
         [SerializeField] private TextMeshProUGUI npcDialogueText;
+        [SerializeField] private float typingSpeed = 0.1f;
 
         private Queue<string> _paragraphs = new Queue<string>();
         private bool _conversationEnded = false;
 
 
+        private Coroutine _dialogueTyping;
+        private bool _isTyping = false;
 
         private void Start()
         {
@@ -38,14 +42,42 @@ namespace _Scripts.NPCs
                     return;
                 }
             }
+
+            var text = _paragraphs.Dequeue();
+
+            if (!_isTyping)
+            {
+                _dialogueTyping = StartCoroutine(TypeText(text));
+            }
             
-            npcDialogueText.text = _paragraphs.Dequeue();
             
             if (_paragraphs.Count == 0)
             {
                 _conversationEnded = true;
             }
             
+        }
+
+        private IEnumerator TypeText(string text)    
+        {
+            _isTyping = true;
+            npcDialogueText.text = "";
+            string originalText = text;
+            string displayText = "";
+            int alphaIndex = 0;
+            
+            string HTML_ALTHPA = "<color=#00000000>";
+            
+            foreach (var letter in text.ToCharArray())
+            {
+                alphaIndex++;
+                npcDialogueText.text = originalText;
+                displayText = npcDialogueText.text.Insert(alphaIndex, HTML_ALTHPA);
+                npcDialogueText.text = displayText;
+                yield return new WaitForSeconds(typingSpeed);
+            }
+
+            _isTyping = false;
         }
 
         private void StartConversation(DialogueSo dialogue)
@@ -92,5 +124,8 @@ namespace _Scripts.NPCs
             Debug.Log("HideDialogue");
             gameObject.SetActive(false);
         }
+
+
+
     }
 }
